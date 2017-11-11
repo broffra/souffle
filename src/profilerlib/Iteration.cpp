37d9ee6721be03b16c298fa3a1bfccae8dbc935e
Brody Franks <25767784+brodyf@users.noreply.github.com>
2017-11-11 22:51:17 +1100

    Implement parsing iteration statistics in the profiler.

--- src/profilerlib/Iteration.cpp
+++ src/profilerlib/Iteration.cpp
@@ -24,7 +24,6 @@ void Iteration::addRule(std::vector<std::string> data, std::string rec_id) {
             rul_rec->setLineNumber();
             rul_rec_map[strTemp] = rul_rec;
         }
-
     } else if (data[0].at(0) == 'n') {
         std::unordered_map<std::string, std::shared_ptr<Rule>>::const_iterator got =
                 rul_rec_map.find(strTemp);
@@ -34,6 +33,17 @@ void Iteration::addRule(std::vector<std::string> data, std::string rec_id) {
         rul_rec->setNum_tuples(std::stol(data[5]) - prev_num_tuples);
         this->prev_num_tuples = std::stol(data[5]);
         rul_rec_map[strTemp] = rul_rec;
+    } else if (data[0].at(0) == 'i') {
+        if (rul_rec_map.find(strTemp) != rul_rec_map.end()) {
+            std::shared_ptr<Rule> rul_rec = rul_rec_map[strTemp];
+            rul_rec->setNum_iterations(std::stod(data[5]) + rul_rec->getNum_iterations());
+        } else {
+            std::shared_ptr<Rule> rul_rec = std::make_shared<Rule>(Rule(data[4], std::stoi(data[2]), rec_id));
+            rul_rec->setNum_iterations(std::stod(data[5]));
+            rul_rec->setLocator(data[3]);
+            rul_rec->setLineNumber();
+            rul_rec_map[strTemp] = rul_rec;
+        }
     }
 }
 
