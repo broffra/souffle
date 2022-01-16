85f82015326ad2d84ccae0869b1e74ee2d3f3a61
Brody Franks <brody@franks.id.au>
2022-01-16 08:59:07 +0000

    Move Python script to sh directory
    Signed-off-by: Brody Franks <brody@franks.id.au>

--- .github/actions/cmake-test/action.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-n_tests, n_chunks, chunk_id = map(int, sys.stdin.readline().split())
-d, m = divmod(n_tests, n_chunks)
-print(f"{chunk_id * d + min(m, chunk_id) + 1},{(chunk_id+1) * d + min(m, chunk_id+1)}")
